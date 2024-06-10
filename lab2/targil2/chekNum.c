b1c28c7d91a1da8e20ad57c265ac2276eedf3398
Yuval Kogan <Yuvalkogan123@gmail.com>
2024-06-10 13:56:45 +0300

    finished targil2

--- lab2/targil2/chekNum.c
+++ lab2/targil2/chekNum.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, int argv[]){
-    int result = argv[0];
-    int i,j;
+int main(int argc, char* argv[]){
+    int result = atoi(argv[1]); 
+    int i,j, firstNumber, secondNumber;
 
-    if (argc < 3){ // Meaning there`s less than 2 numbers given as arguments to compare with.
+    if (argc < 4){ // Meaning there are less than 2 numbers given as arguments to compare with.
         printf("2 numbers do not exist.\n");
         exit(1);
     }
 
-    for (i=0;i<argc;i++){
+    for (i=2;i<argc;i++){
         for (j=i+1;j<argc;j++){
-            if (argv[i] + argv[j] == result ){
-                // TODO: coninute here and check the condition it might cause a out of bounds error.
+            firstNumber = atoi(argv[i]);
+            secondNumber = atoi(argv[j]);
+            if (firstNumber + secondNumber == result ){
+                printf("2 numbers exist: %d + %d = %d",firstNumber, secondNumber, result);
+                return 0;
             }
         }
     }
-
-
+    printf("2 numbers do not exist.\n");
     return 0;
 }
---
+++ lab2/targil2/chekNum.sh
@@ -0,0 +1,5 @@
+cd ~/operatingSystems/lab2/targil2
+cc chekNum.c -o chekNum
+./chekNum 100 5 6 7 50 50
+./chekNum 100 1 2 3
+
