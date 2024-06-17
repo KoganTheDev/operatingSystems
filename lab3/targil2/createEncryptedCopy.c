828dc73057f1010b1c0a1c7686e614b3d6091ac2
Yuval Kogan <Yuvalkogan123@gmail.com>
2024-06-17 13:23:14 +0300

    going back to the vm

---
+++ lab3/targil2/createEncryptedCopy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include  <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]){
+    chr[argv[2]] buffer;
+    int mainFile;
+
+    if (argc != 4){
+        printf("The function must get 4 arguments.\n");
+        exit(1);
+    }
+
+    if (mainFile = open())
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ lab3/targil2/createEncryptedCopy.sh
@@ -0,0 +1,3 @@
+cd ~/operatingSystems/lab3/targil2
+cc createEncryptedCopy.c -o createEncryptedCopy
+./createEncryptedCopy a.txt 2 xx
