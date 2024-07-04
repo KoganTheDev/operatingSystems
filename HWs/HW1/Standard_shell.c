f0dff87284126f29e6e05d0a2498505d2d1908dc
Yuval Kogan <Yuvalkogan123@gmail.com>
2024-07-04 13:18:44 +0300

    Standard_shell works correctly.

--- HWs/HW1/Standard_shell.c
+++ HWs/HW1/Standard_shell.c
@@ -9,7 +9,7 @@
 #include <errno.h>
 
 #define SIZE_OF_INPUT 256
-#define MAX_ARGS 4
+#define MAX_ARGS 5
 
 // Function declarations
 void createNewDir();
@@ -29,9 +29,9 @@ void parseUserInput(char* userInput, char* arguments[], int* argumentsIndex);
  * @param argv Array of command-line argument strings.
  * @return Exit status of the program.
  */
-void main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 
-    if (argc != 1){
+    if (argc != 1) {
         printf("To run the \"Math_shell\" you need to give one argument which is \"Math\".\n");
         exit(1);
     }
@@ -43,13 +43,13 @@ void main(int argc, char* argv[]){
 
     // Ensure the directory ./Commands exists
     createNewDir();
-    
+
     // Main command processing loop
-    while (1){
+    while (1) {
         printf("StandardShell > ");
-        
+
         // Grab input from the user until you get a '\n' character.
-        if ((fgets(userInput, SIZE_OF_INPUT, stdin)) == NULL){
+        if ((fgets(userInput, SIZE_OF_INPUT, stdin)) == NULL) {
             printf("fgets in \"Standard_shell\" has failed to grab input.\n");
             exit(1);
         }
@@ -60,69 +60,56 @@ void main(int argc, char* argv[]){
         // Parse user input into arguments array
         parseUserInput(userInput, arguments, &argumentsIndex);
 
-        // Special case: the user didn`t insert input therefore allow him to try inserting once again.
-        if (argumentsIndex == 0){
+        // Special case: the user didn't insert input therefore allow him to try inserting once again.
+        if (argumentsIndex == 0) {
             printf("No input received.\n");
             continue;
         }
 
         // Fork a child process to execute the command.
-        if ((pid = fork()) < 0){
-                printf("Forking has failed for the Standard_Shell.\n");
-                exit(1);
+        if ((pid = fork()) < 0) {
+            printf("Forking has failed for the Standard_Shell.\n");
+            exit(1);
         }
 
         // Look for the exact match function to run
-        if ((strcmp(arguments[0], "exit")) == 0){
-            if (pid == 0){
+        if ((strcmp(arguments[0], "exit")) == 0) {
+            if (pid == 0) {
                 execlp("./functions/Standard_Shell_functions/exit", "exit", NULL);
-            }
-            else{ // Parent`s clause
+            } else { // Parent's clause
                 break;
             }
-        }
-        else if ((strcmp(arguments[0], "Math")) == 0){
-            if (pid == 0){
-                execlp("./functions/Standard_Shell_functions/Math", "Math", NULL); 
-            } 
-        }
-        else if ((strcmp(arguments[0], "Logic")) == 0){
-            if (pid == 0){
-                execlp("./functions/Standard_Shell_functions/Logic", "Logic", NULL); 
+        } else if ((strcmp(arguments[0], "Math")) == 0) {
+            if (pid == 0) {
+                execlp("./functions/Standard_Shell_functions/Math", "Math", NULL);
             }
-        }
-        else if ((strcmp(arguments[0], "String")) == 0){
-            if (pid == 0){
-                execlp("./functions/Standard_Shell_functions/String", "String", NULL); 
+        } else if ((strcmp(arguments[0], "Logic")) == 0) {
+            if (pid == 0) {
+                execlp("./functions/Standard_Shell_functions/Logic", "Logic", NULL);
             }
-        }
-        else{
+        } else if ((strcmp(arguments[0], "String")) == 0) {
+            if (pid == 0) {
+                execlp("./functions/Standard_Shell_functions/String", "String", NULL);
+            }
+        } else {
             // Try executing a regular shell function.
-            if (pid == 0){
-                if (argumentsIndex == 1){
-                    execlp(arguments[0], arguments[0], NULL); 
-                }
-                else if (argumentsIndex == 2){
-                    execlp(arguments[0], arguments[0], arguments[1], NULL); 
-                }
-                else if (argumentsIndex == 3){
-                    execlp(arguments[0], arguments[0], arguments[1], arguments[2], NULL); 
-                }
-                else{
-                    execlp(arguments[0], arguments[0], arguments[1], arguments[2], arguments[3], NULL);
-                }
-                printf("Not Supported\n");
+            if (pid == 0) {
+                arguments[argumentsIndex] = NULL; // Null-terminate the arguments array
+                execvp(arguments[0], arguments);
+                printf("Not Supported\n"); // Print "Not Supported" if execvp fails
+                exit(1);
             }
         }
 
         // Exit the child process if a function was executed
-        if (pid == 0){
+        if (pid == 0) {
             exit(0);
         }
-        
+
         wait(NULL); // Waits for the child process to end
     }
     wait(NULL); // Wait for exit to end.
+    return 0;
 }
 
 /**
@@ -135,20 +122,18 @@ void main(int argc, char* argv[]){
  * @param None.
  * @return None.
  */
-void createNewDir(){
+void createNewDir() {
     DIR* dir = opendir("./Commands");
 
-    if (dir){
+    if (dir) {
         closedir(dir); // Closes the file descriptor
         printf("The directory ./Commands already exists.\n");
-    }
-    else if (errno == ENOENT){ // ENOENT in this case will say that there`s no entry for this dir, therefore we`ll create it.
-        if(mkdir("./Commands",0700) == -1){
+    } else if (errno == ENOENT) { // ENOENT in this case will say that there's no entry for this dir, therefore we'll create it.
+        if (mkdir("./Commands", 0700) == -1) {
             printf("Making the new directory has failed.\n");
             exit(1);
-        }
-        else{
-        printf("Commands dir created.\n");
+        } else {
+            printf("Commands dir created.\n");
         }
     }
 }
@@ -164,17 +149,16 @@ void createNewDir(){
  * @param argumentsIndex Pointer to the index of arguments array.
  * @return None.
  */
-void parseUserInput(char* userInput, char *arguments[], int* argumentsIndex){
+void parseUserInput(char* userInput, char *arguments[], int* argumentsIndex) {
     *argumentsIndex = 0;
 
     char* token = strtok(userInput, " ");
 
-    arguments[*argumentsIndex] = token;
-    (*argumentsIndex)++;
-
-    while (token != NULL && *argumentsIndex < MAX_ARGS){
+    while (token != NULL && *argumentsIndex < MAX_ARGS - 1) {
         arguments[*argumentsIndex] = token;
-        token = strtok(NULL, " ");
         (*argumentsIndex)++;
+        token = strtok(NULL, " ");
     }
+
+    arguments[*argumentsIndex] = NULL; // Null-terminate the arguments array
 }
