8e5a75ae3e85f9835e97180dbde873c974cb5c7f
Yuval Kogan <Yuvalkogan123@gmail.com>
2024-07-04 14:12:22 +0300

    rewrote DectoBin algorithm so it handles negative numbers as well.

--- HWs/HW1/functions/Logic_Shell_functions/DectoBin.c
+++ HWs/HW1/functions/Logic_Shell_functions/DectoBin.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 
 #define DIGITS_IN_INT 32
@@ -20,41 +19,32 @@
  * Returns:
  * @return - (int): The binary representation of the number in integer format.
  */
-int main(int argc, char* argv[]){
-
-    int base10 = DIGITS_IN_INT;
-    int numberAsBinary = 0, noMoreLeadingZeros = 0;
-    int dectoBinString[DIGITS_IN_INT] = {0};
+int main(int argc, char* argv[]) {
     int number = atoi(argv[1]);
-    int numberCopy = number;
+    unsigned int numberCopy = (unsigned int)number;
+    int dectoBinString[DIGITS_IN_INT] = {0};
+    int noMoreLeadingZeros = 0;
     int i;
 
     // Convert to an integer that represents number in its binary format
-    for (i = DIGITS_IN_INT - 1; i >= 0; i--){
-        if ((number % 2) == 1){
-            numberAsBinary += 1 * pow(10, (double) base10);
-            dectoBinString[i] = 1;
-        }
-
-        number /= 2;
-        base10 -= 1;
+    for (i = DIGITS_IN_INT - 1; i >= 0; i--) {
+        dectoBinString[i] = numberCopy & 1;
+        numberCopy >>= 1;
     }
 
     // Print the binary representation, removing leading zeros
-    for (i = 0; i < DIGITS_IN_INT; i++){
-        if (numberCopy == 0){
+    for (i = 0; i < DIGITS_IN_INT; i++) {
+        if (number == 0) {
             printf("0");
             break;
-        }
-        else if (noMoreLeadingZeros){
+        } else if (noMoreLeadingZeros) {
             printf("%d", dectoBinString[i]);
-        }
-        else if (dectoBinString[i] == 1){
+        } else if (dectoBinString[i] == 1) {
             printf("%d", dectoBinString[i]);
             noMoreLeadingZeros = 1; 
         }
     }
     printf("\n");
 
-    return numberAsBinary;
+    return 0;
 }
