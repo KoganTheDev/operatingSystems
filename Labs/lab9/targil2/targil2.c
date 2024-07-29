2b6b2a106cce152afab8d447e73c9f15edc8c7a6
Yuval Kogan <Yuvalkogan123@gmail.com>
2024-07-29 12:48:21 +0300

    first prototype targil2 , continue from here

--- Labs/lab9/targil2/targil2.c
+++ Labs/lab9/targil2/targil2.c
@@ -5,101 +5,109 @@
 #include <semaphore.h>
 
 void* watchMovie(void* p);
-void enterRoom();
-void leaveRoom();
+void isTheaterFull();
+void isRoomFull();
 
-#define peopleAllowedInOneRoom 5 // Maximum people per movie hall.
-#define N 5 // Number of movie halls.
-#define amountOfThreads 20 // Number of threads.
+#define peopleAllowedInOneRoom 5 // Represents how many people can enter into a movie hall at once.
+#define N 5 // Represents the movie halls.
+#define amountOfThreads 20 // Represents the amount of threads.
 
-int occupiedMovieHalls = 0; // Number of occupied movie halls.
-int peopleInOneMovieRoom = 0; // Number of people currently in the room.
-int hallNumber = 1; // Current movie hall number.
+int occupiedMovieHalls = 0;
+int peopleInOneMovieRoom = 0;
+int hallNumber = 1;
 
-sem_t wantToSeeMovie; // Semaphore to control entry into the movie hall.
-sem_t hallNumberMutex; // Semaphore for managing hall number access.
-sem_t peopleInOneMovieRoomMutex; // Semaphore for managing room occupancy.
+sem_t wantToSeeMovie;
+sem_t hallNumberMutex;
+sem_t peopleInOneMovieRoomMutex;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]){
     pthread_t threads[amountOfThreads];
     int threadID[amountOfThreads], i;
     int threadCheck;
 
-    sem_init(&wantToSeeMovie, 0, 0); // Start with 0, as people need to wait for a room.
+    sem_init(&wantToSeeMovie, 0, 2);
     sem_init(&hallNumberMutex, 0, 1);
     sem_init(&peopleInOneMovieRoomMutex, 0, 1);
 
-    // Create thread ID numbers.
-    for (i = 0; i < amountOfThreads; i++) {
+    // Create id number for each thread.
+    for (i = 0; i < amountOfThreads; i++){
         threadID[i] = i;
     }
 
-    // Create threads.
-    for (i = 0; i < amountOfThreads; i++) {
-        threadCheck = pthread_create(&threads[i], NULL, &watchMovie, (void*)&threadID[i]);
-        if (threadCheck != 0) {
-            fprintf(stderr, "Creating thread %d has failed.\n", i);
+    // Create the threads and send them to the functions.
+    for (i = 0; i < amountOfThreads; i++){
+        threadCheck = pthread_create(&threads[i], NULL, &watchMovie, (void*) &threadID[i]);
+        if (threadCheck != 0){
+            printf("Creating thread %d has failed.\n", i);
             exit(EXIT_FAILURE);
         }
     }
 
-    // Join threads.
-    for (i = 0; i < amountOfThreads; i++) {
+    // join the threads and end the main thread.
+    for (i = 0; i < amountOfThreads; i++){
         pthread_join(threads[i], NULL);
     }
 
-    // Destroy semaphores.
-    sem_destroy(&wantToSeeMovie);
+    sem_destroy(&wantToSeeMovie); 
     sem_destroy(&hallNumberMutex);
     sem_destroy(&peopleInOneMovieRoomMutex);
 
     return 0;
 }
 
-void* watchMovie(void* p) {
-    int threadID = *(int*)p;
+
+void* watchMovie(void* p){
+    int threadID = *(int*) p;
     printf("Person %d wants to watch movie\n", threadID);
 
-    while (1) {
-        // Try to enter a room if there is space.
-        enterRoom();
-
+    while (1){
+        
+        // try to enter - only when there`s an empty room and 2 poeple, allow entrance.
+        if (peopleInOneMovieRoom < 2){
+            sem_wait(&peopleInOneMovieRoomMutex);
+            peopleInOneMovieRoom++;
+            sem_post(&peopleInOneMovieRoomMutex);
+        }
+        else{
+            continue;
+        }
+        
         sem_wait(&wantToSeeMovie);
 
         sem_wait(&peopleInOneMovieRoomMutex);
         peopleInOneMovieRoom++;
-        if (peopleInOneMovieRoom == peopleAllowedInOneRoom) {
-            leaveRoom(); // Room is full, move to the next hall.
-        }
+
+        isTheaterFull(); // If the theater is full try to get into the first room.
+        isRoomFull();
+
         sem_post(&peopleInOneMovieRoomMutex);
 
-        printf("Person %d watching movie in room %d\n", threadID, hallNumber);
+        printf("Person %d watch movie in room %d\n", threadID, hallNumber);
         sleep(2);
         printf("Person %d END watch movie\n", threadID);
+
+    }
+}
+
+void isTheaterFull(){
+    // Room is full and there`s no more place in the last room.
+    if (hallNumber == N && (peopleInOneMovieRoom == peopleAllowedInOneRoom)){
+        sem_wait(&hallNumberMutex);
+        hallNumber = 1;
+        peopleInOneMovieRoom = 0;
+        sem_post(&hallNumberMutex);
     }
 
     return NULL;
 }
 
-void enterRoom() {
-    sem_wait(&peopleInOneMovieRoomMutex);
-    if (peopleInOneMovieRoom == 0 && occupiedMovieHalls < N) {
-        occupiedMovieHalls++;
+void isRoomFull(){
+    if (peopleInOneMovieRoom == peopleAllowedInOneRoom){
+        peopleInOneMovieRoom = 0;
+        hallNumber++;
+
+        // Make 2 people wait, so they can get inside together.
+        sem_post(&wantToSeeMovie);
         sem_post(&wantToSeeMovie);
     }
-    sem_post(&peopleInOneMovieRoomMutex);
-}
-
-void leaveRoom() {
-    sem_wait(&hallNumberMutex);
-    hallNumber++;
-    if (hallNumber > N) {
-        hallNumber = 1;
-    }
-    peopleInOneMovieRoom = 0;
-    sem_post(&hallNumberMutex);
-
-    // Signal that two more people can enter.
-    sem_post(&wantToSeeMovie);
-    sem_post(&wantToSeeMovie);
 }
